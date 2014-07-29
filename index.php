f606b71d41b2393176aec38710764d10e958fa67
Mario Hsieh <mariohsieh@gmail.com>
2014-07-29 15:44:36 -0700

    search and count box works

--- index.php
+++ index.php
@@ -7,35 +7,55 @@
 	
 	<!-- css links -->
 	<!-- <link rel="stylesheet" href="libs/reset.css" /> <!-- meyer reset -->
-	<!--<link rel="stylesheet" href="css/style.css" />-->
+	<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-	<h2>php web proxy version</h2>
+	<h2>clinical trials</h2>
+	
+	<!-- search & filter -->
+	<p>Please input search topics</p>
+	<input id="searchTopic" type="search" placeholder="search studies" />
+	<p>Input between 0 - 100 results</p>
+	<input id="resultsCount" type="number" min="0" step="1" max="100" pattern="\d+" />
 	<button>click</button>
+	
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script>
 		$(document).ready(function() {
-			var path = "ct2/results?term=Stroke+AND+Brain&Search=Searc&displayxml=true";
 			
-			var url = "http://localhost/projects/zephyr-health/process.php?path=" + encodeURIComponent(path);
-			
+			// declare url and path for proxy server
+			var url = "http://localhost/projects/zephyr-health/process.php?path=";
+			var path = "ct2/results?term=&Search=Search&displayxml=true";
+
 			$(document).on("click", "button", function() {
+				var searchTopic = $("#searchTopic").val().trim();
 				
+				// if user inputs search items
+				if (searchTopic != '') {
+					searchTopic = searchTopic.split(" ").join("+");
+					path = "ct2/results?term="+searchTopic+"&Search=Search&displayxml=true";
+				}
+							
+				// if user inputs results number
+				var resultsCount = $("#resultsCount").val().trim();
+				if (resultsCount != '') {
+					resultsCount = "&count="+resultsCount.toString();
+					path += path+resultsCount;
+				}
+
+				console.log(path);
+
 				$.ajax({
 					dataType: "json",
-					//url: "process.php",
-					//data: {url: url},
-					url: url,
-					//url: "sample-large.xml",
-					//headers: { 'Access-Control-Allow-Origin': '*' },
-					//crossDomain: true,
+					url: url+encodeURIComponent(path),
 					error: function() {
-						console.log("Error!");
+						console.log("Error in accessing data!");
 					},
 					success: function(data) {
 						console.log(data);
 					}				
-				});		
+				});	
+				
 			});
 		});
 	</script>
