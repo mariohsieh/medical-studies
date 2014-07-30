9848cd692924e5e3ec8089d33cd2026838d3cf26
Mario Hsieh <mariohsieh@gmail.com>
2014-07-30 12:23:42 -0700

    1st chart with data working

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
 	var url = "http://localhost/projects/zephyr-health/process.php?path=";
 	var path = "ct2/results?term=&Search=Search&displayxml=true";
 
-	$(document).on("click", "button", function() {
+	//// helper functions ////
+	function fetchResults() {
 		var searchTopic = $("#searchTopic").val().trim();
 		
 		// if user inputs search items
@@ -29,9 +30,122 @@ $(document).ready(function() {
 				console.log("Error in accessing studies.");
 			},
 			success: function(data) {
-				console.log(data);
+				displayStudies(data);
 			}				
-		});	
+		});			
+	}
+
+	function displayStudies(info) {
+		console.log(info);
 		
+		var catNames = [];
+		var catValues = [];
+		
+		var items = info.clinical_study;
+
+		// find all the categories from results
+		for (item in items) {
+			console.log(items[item].status);
+			
+			if (catNames.length != 0) {
+				for (var i=0;i<catNames.length;i++) {
+					if (items[item].status == catNames[i])
+						break;
+					else if (i == catNames.length-1)
+						catNames.push(items[item].status);
+				}
+			} else
+				catNames.push(items[item].status);
+		}
+		
+		// find all values for each categories
+		for (var i=0;i<catNames.length;i++) {
+			catValues[i] = 0;
+			for (item in items) {
+				if (items[item].status == catNames[i])
+					catValues[i]++;
+			}
+		}
+		
+		console.log(catNames);
+		console.log(catValues);
+		
+		var options = {
+			chart: {
+				type: 'column'
+			},
+			title: {
+				text: 'Status Chart'
+			},
+			subtitle: {
+				text: 'Source: clinicaltrials.gov'
+			},
+			yAxis: {
+				min: 0,
+				title: {
+					text: '# of Studies'
+				}
+			},
+			xAxis: {
+				categories: catNames,
+				title: {
+					text: null
+				}
+			},
+			legend: {
+				layout: 'vertical',
+				align: 'center',
+				verticalAlign: 'bottom',
+				x: 0,
+				y: 0,
+				floating: false,
+				borderWidth: 1
+			},
+			series: [{
+				name: 'Status',
+				data: catValues
+			}]
+		};
+		
+		// append options to make charts
+		$("#charts").highcharts(options);		
+	}
+	
+	
+	//// event listeners ////
+	$(document).on("click", "button", function() {
+		fetchResults();
 	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
